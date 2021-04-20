643d0de4346a0a7f9e6cb67e8682203cf6df0d39
Bernardo Mattos Netto <61763229+sl4k3r1@users.noreply.github.com>
2021-04-19 21:45:44 -0300

    Deu ruim nos arquivos
    O Gabriel vai repostar os arquivos corretos e temos q abrir novas branchs, plz!!

--- 2021-1-dev11-1/README.md
+++
@@ -1,3 +0,0 @@
-# 2021-1-dev11
-
-Para acessar o site :https://sl4k3r1.github.io/2021-1-dev11/
--- 2021-1-dev11-1/js/produto.js
+++
@@ -1,9 +0,0 @@
-/* js/produto.js */
-
-function troca (imagem, raca){
-	var grande = document.querySelectorAll('.album img')[0];
-	var nome = document.querySelectorAll('.album h2')[0];
-	grande.src = imagem;
-	nome.innerHTML = raca;
-	
-}
